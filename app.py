2b73d5e42566e295c4fabcf6f11ac73e6ed954c8
Abhishek Sharma <as19103@gmail.com>
2019-08-14 16:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,56 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    reply = {
-        "fulfillmentText": response,
-    }
+    result = {} # an empty dictionary
 
-    return jsonify(reply)
+        # fulfillment text is the default response that is returned to the dialogflow request
+        result["fulfillmentText"] = "your response message here"
+
+        # you can also make rich respones like basic card, simple responses, list, table card etc.
+        # you can refer this for rich response formats
+        # https://github.com/dialogflow/fulfillment-webhook-json
+
+        # you can also use custom payloads for different services like messenger or google assistant
+        # below is an example of google assistant payload
+        # the following paylod contains a simple response, a basic card and some suggestion chips.
+
+        reply["payload"] = {
+            "google": {
+            "expectUserResponse": True,
+            "richResponse": {
+                "items": [
+                {
+                    "simpleResponse": {
+                        "displayText": 'text to be displayed',
+                        "textToSpeech": 'text that will be used for text to speech'
+                    }
+                },
+                {
+                    "basicCard": {
+                        "title": "card title",
+                        "subtitle": "card subtitle",
+                        "imageDisplayOptions": "WHITE"
+                    }
+                }
+                ],
+                "suggestions": [
+                    {
+                        "title": "chip 1"
+                    },
+                    {
+                        "title": "chip 2"
+                    }
+                ],
+            }
+            }
+        }
+
+        # jsonify the result dictionary
+        # this will make the response mime type to application/json
+        result = jsonify(result)
+
+        # return the result json
+        return make_response(result)
 
 def results():
     # build a request object
